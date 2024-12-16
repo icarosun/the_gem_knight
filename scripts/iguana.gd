48edfd8e57783c1271b150428ccf779b32bd4215
Sun <icaropereirasun@gmail.com>
2024-12-16 10:41:38 -0400

    add: sword effect

--- scripts/iguana.gd
+++ scripts/iguana.gd
@@ -1,25 +1,20 @@
-extends CharacterBody2D
+extends Area2D
 
+var dead = false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready() -> void:
+	pass
 
+func _process(delta: float) -> void:
+	if dead == false:
+		$AnimatedSprite2D.play("idle")
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Sword"):
+		dead = true
+		$AnimatedSprite2D.play("Destroyed")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	move_and_slide()
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "Destroyed":
+		queue_free();
+		dead = false
